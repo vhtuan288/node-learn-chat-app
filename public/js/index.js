f2ae71b2b21fd40ad7d0466cc5fb884572434350
Vu Huy Tuan <vh.tuan288@gmail.com>
2018-04-20 15:44:07 +0700

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    to: 'Tuan',
+    text: 'That work for me.'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+})
--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Anonymous',
+    text: 'See you then',
+    createAt: '12032016'
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
