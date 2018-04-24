3b158aa63567ec75f6a4d7227d1df890fa6e97bf
Vu Huy Tuan <vh.tuan288@gmail.com>
2018-04-24 20:26:30 +0700

    Add form for messages and how incomming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
-})
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+socket.emit('createdMessage', {
+  from: 'Vu',
+  text: 'Hi'
+}, function (data) {
+  console.log('Got it!', data);
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createdMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
+
+  });
+});
